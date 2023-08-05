4559d931206deafc2d1f22be33ef5d52187b4426
Shehan Maxwell <shehanbbn@gmail.com>
2023-08-05 16:44:28 +0530

    modified App.js in src

--- src/App.js
+++ src/App.js
@@ -16,7 +16,19 @@ function App() {
     getUsers();
   }, []);
 
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      
+      {users.map((user) => {
+        return (
+          <div>
+            <h1>Name: {user.name}</h1>
+            <h1>Age: {user.age}</h1>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default App;
